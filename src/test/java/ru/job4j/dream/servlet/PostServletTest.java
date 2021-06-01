4067476cc3c2ed5797caf19c3fd2cb29ec773899
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2021-06-02 00:14:47 +0700

    1. Написать тест на PostServlet. [#2518] UPD

--- src/test/java/ru/job4j/dream/servlet/PostServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/PostServletTest.java
@@ -9,15 +9,17 @@ import ru.job4j.dream.store.MemStore;
 import ru.job4j.dream.store.PsqlStore;
 import ru.job4j.dream.store.Store;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.junit.Assert.assertFalse;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(PsqlStore.class)
@@ -25,9 +27,9 @@ public class PostServletTest {
 
     @Test
     public void whenAddPostThenStoreIt() throws IOException {
-        Store validate = MemStore.instOf();
+        Store memStore = MemStore.instOf();
         PowerMockito.mockStatic(PsqlStore.class);
-        when(PsqlStore.instOf()).thenReturn(validate);
+        when(PsqlStore.instOf()).thenReturn(memStore);
         HttpServletRequest req = mock(HttpServletRequest.class);
         HttpServletResponse resp = mock(HttpServletResponse.class);
         when(req.getParameter("op")).thenReturn("edit");
@@ -35,11 +37,40 @@ public class PostServletTest {
         when(req.getParameter("name")).thenReturn("CEO");
         when(req.getParameter("description")).thenReturn("some description");
         new PostServlet().doPost(req, resp);
-        var it = validate.findAllPosts().stream().iterator();
+        var it = memStore.findAllPosts().stream().iterator();
         assertThat(it.next().getName(), is("Junior Java Job"));
         assertThat(it.next().getName(), is("Middle Java Job"));
         assertThat(it.next().getName(), is("Senior Java Job"));
         assertThat(it.next().getName(), is("CEO"));
         assertFalse(it.hasNext());
+    }
+
+    @Test
+    public void whenGetForAddOrEditThenForward() throws ServletException, IOException {
+        Store memStore = MemStore.instOf();
+        PowerMockito.mockStatic(PsqlStore.class);
+        when(PsqlStore.instOf()).thenReturn(memStore);
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+        RequestDispatcher dispatcher = mock(RequestDispatcher.class);
+        when(req.getRequestDispatcher("WEB-INF/post/edit.jsp")).thenReturn(dispatcher);
+
+        new PostServlet().doGet(req, resp);
+        verify(dispatcher).forward(req, resp);
+    }
+
+    @Test
+    public void whenGetForDelThenRedirect() throws ServletException, IOException {
+        Store memStore = MemStore.instOf();
+        PowerMockito.mockStatic(PsqlStore.class);
+        when(PsqlStore.instOf()).thenReturn(memStore);
+        HttpServletRequest req = mock(HttpServletRequest.class);
+        when(req.getParameter("op")).thenReturn("del");
+        when(req.getParameter("id")).thenReturn("1");
+        HttpServletResponse resp = mock(HttpServletResponse.class);
+
+        new PostServlet().doGet(req, resp);
+        verify(resp).sendRedirect(req.getContextPath() + "/posts.do");
+        assertNull(memStore.findPostById(1));
     }
 }
