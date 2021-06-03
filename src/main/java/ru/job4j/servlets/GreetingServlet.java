c7f8ce4452ad533b25489efa65268852e3764465
ForLearningAtJob4J <youcanwriteme@yandex.ru>
2021-06-03 23:25:02 +0700

    4.1. JSON формат. [#238554]

--- src/main/java/ru/job4j/servlets/GreetingServlet.java
+++ src/main/java/ru/job4j/servlets/GreetingServlet.java
@@ -1,11 +1,14 @@
 package ru.job4j.servlets;
 
+import org.json.JSONObject;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.stream.Collectors;
 
 public class GreetingServlet extends HttpServlet {
 
@@ -16,6 +19,16 @@ public class GreetingServlet extends HttpServlet {
         String name = req.getParameter("name");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
         writer.println("Nice to meet you, " + name);
+        writer.flush();
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/json");
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        writer.println(new JSONObject(req.getReader().lines().collect(Collectors.joining())));
         writer.flush();
     }
 }
